33cead9cd5ef4dd3d9f63598fd948518ea8971a5
Bogdan9871 <bogdanandrei9871@gmail.com>
2022-11-07 14:46:42 +0200

    changes

--- Gramatici generative/gramatici.py
+++ Gramatici generative/gramatici.py
@@ -1,16 +1,20 @@
+import re
+
+
 class gramatica:  # G=(VN, VT,S,P)
     vn = ""
     vt = ""
     simbolStart = ""
     productie = ""
     numar = int()
+    productieDreapta = []
+    productieStanga = []
 
     def __init__(self):
         with open(
             "C:\\Users\\bogda\\Desktop\TEME LFC\\Gramatici generative\\VN.txt", "r"
         ) as f:
-            self.vn = f.readlines()
-            self.vn = "".join(self.vn)  # transformare in string
+            self.vn = [line.rstrip("\n") for line in f]  # transformare in string
         with open(
             "C:\\Users\\bogda\\Desktop\\TEME LFC\\Gramatici generative\\VT.txt", "r"
         ) as f:
@@ -28,14 +32,47 @@ class gramatica:  # G=(VN, VT,S,P)
             "r",
         ) as f:
             self.productie = [line.rstrip("\n") for line in f]
+        for productie in self.productie:
+            self.productieDreapta.append(
+                re.findall("[^->]+\Z", productie)
+            )  # Creare lista cu elemente de modificat
+
+            self.productieStanga.append(
+                re.findall("^[^->]+", productie)
+            )  ##Creare lista cu elemente de cautat
+        self.productieStanga = [
+            s for S in self.productieStanga for s in S
+        ]  # convertire in lista simpla
+        self.productieDreapta = [s for S in self.productieDreapta for s in S]
 
     def print(self):
         print("G=(", self.vn, self.vt, self.simbolStart, ",P)")
         print("P= ", self.productie)
 
+    def verificare(self):
+
+        if bool(set(self.vn) & set(self.vt)) == True:  # verifica intersectia
+            print("Prima conditie nu este indeplinita")
+        else:
+            print("Prima conditie este indeplinita")
+        if (self.simbolStart in self.vn) == True:
+            print("A doua conditie este indeplinita")
+        else:
+            print("A doua conditie nu este indeplinita")
+        if ("S" in self.productieStanga) == True:
+            print("A patra condtie este indeplinita")
+        else:
+            print("A patra condtie nu este indeplinita")
+
+        if all(ch in self.vn for ch in self.productieStanga) == True:
+            print("A cincea condtie este indeplinita")
+        else:
+            print("A cincea conditie nu este indeplinita")
+
 
 def startAlgorithm():
     g = gramatica()
+    g.verificare()
     g.print()
 
 
